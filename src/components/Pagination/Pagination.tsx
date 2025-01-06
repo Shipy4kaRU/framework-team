3332835775dd97abbe58e7f3840b43a5b409a650
Danil Shipy4ka <105577586+Shipy4kaRU@users.noreply.github.com>
2025-01-06 23:03:30 +0400

    lil fixed for pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,34 +1,51 @@
 import { FC, useState, Fragment } from 'react';
 import styles from './styles.module.scss';
 import { getDisplayedPages } from '../../helpers/getDispayedPages';
+import icons from '../../icons/icons_sprite.svg';
 
 interface IPaginationProps {
   totalPages: number;
 }
 
 const Pagination: FC<IPaginationProps> = ({ totalPages = 10 }) => {
-  const [currentPage, setCurrentPage] = useState(4); // стартовая страница
-
-  const handlePageClick = (event: { selected: number }) => {
-    setCurrentPage(event.selected);
-  };
+  const [currentPage, setCurrentPage] = useState(4);
 
   const pages = getDisplayedPages(currentPage, totalPages);
 
   return (
     <div className={styles.pagination}>
+      <button
+        className={styles.button}
+        onClick={() => {
+          if (currentPage > 1) setCurrentPage((state) => state - 1);
+        }}
+      >
+        <svg className={styles.prev}>
+          <use xlinkHref={`${icons}#arrow`} />
+        </svg>
+      </button>
       {pages.map((page, index) => (
         <Fragment key={page}>
           {index > 0 && pages[index] - pages[index - 1] > 1 && <span>...</span>}
           <button
             className={`${styles.page} ${currentPage === page ? styles.active : ''}`}
-            onClick={() => handlePageClick({ selected: page })}
+            onClick={() => setCurrentPage(page)}
             disabled={currentPage === page}
           >
             {page}
           </button>
         </Fragment>
       ))}
+      <button
+        className={styles.button}
+        onClick={() => {
+          if (currentPage < 10) setCurrentPage((state) => state + 1);
+        }}
+      >
+        <svg className={styles.next}>
+          <use xlinkHref={`${icons}#arrow`} />
+        </svg>
+      </button>
     </div>
   );
 };
