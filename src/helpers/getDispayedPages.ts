633abde145935c544938d87f0477b963678cf54e
Danil Shipy4ka <105577586+Shipy4kaRU@users.noreply.github.com>
2025-01-10 11:13:20 +0400

    fixed to pixel perfect again

--- src/helpers/getDispayedPages.ts
+++ src/helpers/getDispayedPages.ts
@@ -1,6 +1,10 @@
 export const getDisplayedPages = (currentPage: number, totalPages: number) => {
   const displayedPages = [];
 
+  if (currentPage === 1 && totalPages > 3) {
+    return [1, 2, 3, totalPages];
+  }
+
   displayedPages.push(1);
 
   if (currentPage > 1) {
