1e4766d5a03d18c7b7ef55faf31433287f40fe8f
Danil Shipy4ka <105577586+Shipy4kaRU@users.noreply.github.com>
2025-01-10 12:23:00 +0400

    add error control for images

--- src/components/UI/Picture/Picture.tsx
+++ src/components/UI/Picture/Picture.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 import styles from './styles.module.scss';
 import { BASE_URL } from '../../../constants/BASE_URL';
 
@@ -8,8 +8,30 @@ interface PictureProps {
 }
 
 const Picture: FC<PictureProps> = ({ pictureLink, isEnter }) => {
+  const [hasError, setHasError] = useState<boolean>(false);
+
+  const errorHandler = () => {
+    setHasError(true);
+  };
+
+  useEffect(() => {
+    const img = new Image();
+    img.onerror = errorHandler;
+    img.src = `${BASE_URL}${pictureLink}`;
+  }, [pictureLink]);
+
   return (
-    <img src={`${BASE_URL}${pictureLink}`} alt="Picture Name" className={`${styles.picture} ${isEnter ? styles.scale : ''}`} />
+    <div className={styles.wrapper} style={{ backgroundColor: '#575757' }}>
+      <img
+        style={{
+          visibility: hasError ? 'hidden' : 'visible',
+        }}
+        src={`${BASE_URL}${pictureLink}`}
+        alt="Picture Name"
+        className={`${styles.picture} ${isEnter ? styles.scale : ''}`}
+        onError={errorHandler}
+      />
+    </div>
   );
 };
 
